73db39191f85838398ed62368782963b7b597b00
lindasantika08 <linda.santika.tif22@polban.ac.id>
2024-03-23 15:16:26 +0700

    ini aku pertama ges

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -11,7 +11,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:frontend_hotel/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('Counter increments smoke issue', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(const MyApp());
 
