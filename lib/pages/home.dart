43e5c742de49bf1ac7c5d6c50bed9f7ff5dd4122
Alya_Angraini <116321705+aruya2707@users.noreply.github.com>
2024-04-30 08:28:17 +0700

    menghapus manager

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -73,48 +73,6 @@ class _HomeState extends State<Home> {
               ),
             ),
             SizedBox(height: 20),
-            // ini yang tampilan login Manajer
-            GestureDetector(
-              // ini pas pencet costomer
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => Manager()),
-                );
-              },
-              child: Container(
-                child: Column(
-                  children: <Widget>[
-                    Image.asset('assets/images/network.png'),
-                    SizedBox(height: 20),
-                    Container(
-                      child: Text(
-                        'Manager',
-                        style: TextStyle(
-                            color: Colors.green[800],
-                            fontWeight: FontWeight.bold),
-                      ),
-                      padding: EdgeInsets.all(7), // jarak ke dalam
-                      decoration: BoxDecoration(
-                        color: Colors.green[100],
-                        borderRadius: BorderRadius.all(Radius.circular(8)),
-                      ),
-                    )
-                  ],
-                ),
-                padding: const EdgeInsets.fromLTRB(20, 30, 20, 20),
-                alignment: Alignment.center,
-                width: 180,
-                height: 180,
-                decoration: BoxDecoration(
-                    color: Colors.grey[100],
-                    borderRadius: BorderRadius.all(Radius.circular(8)),
-                    border: Border.all(
-                        width: 1.0, color: Colors.grey.withOpacity(0.5)),
-                    boxShadow: [BoxShadow(color: Colors.grey, blurRadius: 1)]),
-              ),
-            ),
-            SizedBox(height: 20),
             // ini yang tampilan login Front Desk
             GestureDetector(
               // ini pas pencet costomer
